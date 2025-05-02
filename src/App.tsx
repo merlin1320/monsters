c0218366138c64faba6895dba1d4d07480eb9bb6
merlin1320 <littlenights447@gmail.com>
2025-05-01 22:15:27 -0600

    Added a modal that will open on selection of a monster

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,74 @@
-import { useState } from 'react'
-import './index.css'
-import axios from 'axios'
+import { useState } from "react";
+import "./index.css";
+import axios from "axios";
 
-const api_url = 'https://3gcxrtjq-3006.usw3.devtunnels.ms/';
+const api_url = "https://3gcxrtjq-3006.usw3.devtunnels.ms/";
 
 export interface Monster {
+  id: number;
   name: string;
+  attributes: Attributes;
+}
+export interface Attributes {
   type: string;
-  traits: string;
-  hitpoints: number;
+  hp: number;
+  traits: Traits;
+}
+export interface Traits {
+  canCatch: boolean;
+  canEvolve: boolean;
+  isBoss: boolean;
 }
 
 function App() {
-  const [data, setData] = useState(null);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [modalOpen, setModalOpen] = useState(false);
-  const [selectedMonster, setSelectedMonster] = useState<any>(null);
+  const [selectedMonster, setSelectedMonster] = useState<Monster>(
+    {} as Monster
+  );
 
-  // Example monster data
-  const monsters = [
-    {
-      name: 'Goblin',
-      type: 'Beast',
-      traits: 'Small, Agile',
-      hitpoints: 12,
-    },
-    // Add more monsters as needed
-  ];
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(api_url);
-      setData(response.data);
+      const response = await axios.get(api_url + 'monsters');
+      setMonsters(response.data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
-  const handleMonsterClick = (monster: any) => {
+  const handleMonsterClick = (monster: Monster) => {
     setSelectedMonster(monster);
     setModalOpen(true);
   };
 
   const handleModalClose = () => {
     setModalOpen(false);
-    setSelectedMonster(null);
+    setSelectedMonster({} as Monster);
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedMonster({ ...selectedMonster, [e.target.name]: e.target.value });
   };
 
+  const handleSave = async () => {
+    if (!selectedMonster) return;
+    try {
+      await axios.patch(`${api_url}${selectedMonster.id}`, selectedMonster);
+      setModalOpen(false);
+      setSelectedMonster({} as Monster);
+      // Optionally, refresh data here
+      // await fetchData();
+    } catch (error) {
+      console.error('Error saving monster:', error);
+    }
+  };
+
   return (
     <>
       <div>
         <button onClick={fetchData}>Fetch Data</button>
-        {data && (
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        )}
+        {monsters && <pre>{JSON.stringify(monsters, null, 2)}</pre>}
         <table>
           <thead>
             <tr>
@@ -67,52 +79,120 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {monsters.map((monster, idx) => (
-              <tr key={idx}>
+            {monsters.map((monster) => (
+              <tr key={monster.id}>
                 <td>
-                  <button style={{ background: 'none', border: 'none', color: 'blue', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => handleMonsterClick(monster)}>
+                  <button
+                    style={{
+                      background: "none",
+                      border: "none",
+                      color: "blue",
+                      textDecoration: "underline",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => handleMonsterClick(monster)}
+                  >
                     {monster.name}
                   </button>
                 </td>
-                <td>{monster.type}</td>
-                <td>{monster.traits}</td>
-                <td>{monster.hitpoints}</td>
+                <td>{monster.attributes.type}</td>
+                <td>
+                  Catch: {monster.attributes.traits.canCatch ? "Yes" : "No"},
+                  Evolve: {monster.attributes.traits.canEvolve ? "Yes" : "No"},
+                  Boss: {monster.attributes.traits.isBoss ? "Yes" : "No"}
+                </td>
+                <td>{monster.attributes.hp}</td>
               </tr>
             ))}
           </tbody>
         </table>
         {/* Modal */}
         {modalOpen && selectedMonster && (
-          <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', background: 'rgba(0,0,0,0.5)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ background: 'white', padding: 24, borderRadius: 8, minWidth: 300 }}>
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100vw",
+              height: "100vh",
+              background: "rgba(0,0,0,0.5)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              style={{
+                background: "white",
+                padding: 24,
+                borderRadius: 8,
+                minWidth: 300,
+              }}
+            >
               <h2>Edit Monster</h2>
               <label>
                 Name:
-                <input name="name" value={selectedMonster.name} onChange={handleInputChange} />
+                <input
+                  name="name"
+                  value={selectedMonster.name}
+                  disabled
+                />
               </label>
               <br />
               <label>
                 Type:
-                <input name="type" value={selectedMonster.type} onChange={handleInputChange} />
-              </label>
-              <br />
-              <label>
-                Traits:
-                <input name="traits" value={selectedMonster.traits} onChange={handleInputChange} />
+                <input
+                  name="type"
+                  value={selectedMonster.attributes.type}
+                  onChange={e => setSelectedMonster({ ...selectedMonster, attributes: { ...selectedMonster.attributes, type: e.target.value } })}
+                />
               </label>
               <br />
               <label>
                 Hitpoints:
-                <input name="hitpoints" value={selectedMonster.hitpoints} onChange={handleInputChange} type="number" />
+                <input
+                  name="hp"
+                  type="number"
+                  value={selectedMonster.attributes.hp}
+                  onChange={e => setSelectedMonster({ ...selectedMonster, attributes: { ...selectedMonster.attributes, hp: Number(e.target.value) } })}
+                />
+              </label>
+              <br />
+              <label>
+                Can Catch:
+                <input
+                  type="checkbox"
+                  checked={selectedMonster.attributes.traits.canCatch}
+                  onChange={e => setSelectedMonster({ ...selectedMonster, attributes: { ...selectedMonster.attributes, traits: { ...selectedMonster.attributes.traits, canCatch: e.target.checked } } })}
+                />
+              </label>
+              <br />
+              <label>
+                Can Evolve:
+                <input
+                  type="checkbox"
+                  checked={selectedMonster.attributes.traits.canEvolve}
+                  onChange={e => setSelectedMonster({ ...selectedMonster, attributes: { ...selectedMonster.attributes, traits: { ...selectedMonster.attributes.traits, canEvolve: e.target.checked } } })}
+                />
+              </label>
+              <br />
+              <label>
+                Is Boss:
+                <input
+                  type="checkbox"
+                  checked={selectedMonster.attributes.traits.isBoss}
+                  onChange={e => setSelectedMonster({ ...selectedMonster, attributes: { ...selectedMonster.attributes, traits: { ...selectedMonster.attributes.traits, isBoss: e.target.checked } } })}
+                />
               </label>
               <br />
               <button onClick={handleModalClose}>Close</button>
+              <button onClick={handleSave} style={{ marginLeft: 8 }}>Save</button>
             </div>
           </div>
         )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
